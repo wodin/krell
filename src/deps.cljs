643de8bca3398a4d2f2e32444a9241db08fc4235
Michael Wood <esiotrot@gmail.com>
2021-10-04 12:49:20 +0200

    Add expo-dev-client and registerRootComponent

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,3 @@
 {:npm-deps {"react-native-tcp-socket" "5.2.0"
-            "react-native-device-info" "8.1.2"}}
+            "react-native-device-info" "8.1.2"
+            "expo-dev-client" "0.5.1"}}
